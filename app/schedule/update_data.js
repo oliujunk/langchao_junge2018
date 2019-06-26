8af3920dfc9c4bb9447e0d11cd7aa22cac883343
oliujunk <413041277@qq.com>
2019-06-26 09:40:29 +0800

    zhaojun

--- app/schedule/update_data.js
+++ app/schedule/update_data.js
@@ -6,8 +6,10 @@ const Subscription = require('egg').Subscription;
 const net = require('net');
 const port = 8888;
 const ip = '119.164.253.229';
-const userId = 463;
-const token = '76996141';
+const userId = 1045;
+const token = '52376242'; // token
+const sbbh = '20190610'; // ID号前缀
+
 
 class UpdateData extends Subscription {
   // 通过 schedule 属性来设置定时任务的执行间隔等配置
@@ -92,7 +94,6 @@ class UpdateData extends Subscription {
       const facId = res.data[i].facId;
       const head = '8888';
       const xxbm = getRadomNum(8);
-      const sbbh = '00000000';
       const sjlx = '00';
       const bysj = '0000000000000000';
       const end = '0304';
